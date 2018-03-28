6f701ac42443da72d77cd07669bbe8a7236611d2
Iznogoud <ze.rui7@gmail.com>
2018-03-28 13:59:08 +0100

    'stm detected as a keyboard'

---
+++ src/cdcacm.h
@@ -0,0 +1,11 @@
+// vim: tabstop=8 softtabstop=8 shiftwidth=8 noexpandtab
+
+#include <libopencm3/usb/cdc.h>
+
+extern const struct usb_interface_descriptor uart_comm_iface[];
+extern const struct usb_interface_descriptor uart_data_iface[];
+extern const struct usb_iface_assoc_descriptor uart_assoc;
+
+void uart_print(char *msg, usbd_device *dev);
+
+void cdcacm_set_config(usbd_device *dev, uint16_t wValue);
